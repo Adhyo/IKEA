9c25e320505aed625e24a878db6f1c8f4fa143fa
Tythan <tsaqiif26@gmail.com>
2024-12-05 13:25:14 +0700

    fixing

--- IKEA/src/Database/DatabaseManager.java
+++ IKEA/src/Database/DatabaseManager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection {
+public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/ikea";
     private static final String USER = "root";
     private static final String PASSWORD = "password";
